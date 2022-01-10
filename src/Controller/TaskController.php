d51477e2bc3f25a40e18128b0a9e12b4a3078d49
Aleksandr <alexandrgaiko88@gmail.com>
2022-01-10 16:59:02 +0200

    adding todo_list_app to github

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/')]
+class TaskController extends AbstractController
+{
+    #[Route('/', name: 'task_index', methods: ['GET', 'POST'])]
+    public function index(TaskRepository $taskRepository): Response
+    {   
+        return $this->render('task/index.html.twig', [
+            'tasks' => $taskRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/completed', name: 'task_completed', methods: ['GET'])]
+    public function completed(TaskRepository $taskRepository): Response
+    {   
+        return $this->render('task/completed.html.twig', [
+            'tasks_completed' => $taskRepository->findBy(['completed' => true]),
+        ]);
+    }
+
+    #[Route('/uncompleted', name: 'task_uncompleted', methods: ['GET'])]
+    public function uncompleted(TaskRepository $taskRepository): Response
+    {   
+        return $this->render('task/uncompleted.html.twig', [
+            'tasks_uncompleted' => $taskRepository->findBy(['completed' => false]),
+        ]);
+    }
+
+    #[Route('/new', name: 'task_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('task_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('task/new.html.twig', [
+            'task' => $task,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'task_show', methods: ['GET'])]
+    public function show(Task $task): Response
+    {
+        return $this->render('task/show.html.twig', [
+            'task' => $task,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'task_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Task $task, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('task_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('task/edit.html.twig', [
+            'task' => $task,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'task_delete', methods: ['POST'])]
+    public function delete(Request $request, Task $task, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$task->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($task);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('task_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Task.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $task;
+
+    #[ORM\Column(type: 'boolean')]
+    private $completed = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTask(): ?string
+    {
+        return $this->task;
+    }
+
+    public function setTask(string $task): self
+    {
+        $this->task = $task;
+
+        return $this;
+    }
+
+    public function getCompleted(): ?bool
+    {
+        return $this->completed;
+    }
+
+    public function setCompleted(bool $completed): self
+    {
+        $this->completed = $completed;
+
+        return $this;
+    }
+}
